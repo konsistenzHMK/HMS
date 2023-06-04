7eedf69ec5e95f426c1cdbf429ab805f5be58181
Shamshad <shamshad@antwalk.com>
2023-06-04 11:29:09 +0530

    update react native run and start commands

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 
 import { AppRegistry, LogBox } from 'react-native'
+// import { BakarRoom as App } from './app/bakar-room/BakarRoom'
 import App from './app/App'
 import { name as appName } from './app.json'
 

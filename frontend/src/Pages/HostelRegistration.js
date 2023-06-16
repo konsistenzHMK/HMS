13a0d182f2fd44ac039856a5b33afd1f788b1703
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-06-16 15:56:56 +0530

    fonrtend updated

--- frontend/src/Pages/HostelRegistration.js
+++ frontend/src/Pages/HostelRegistration.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios, { all } from 'axios';
-
+import Dropdown from 'react-dropdown';
 
 const App = () => {
   const [category1, setCategory1] = useState('');
@@ -159,39 +159,41 @@ const App = () => {
       console.log('Form submitted successfully!');
       axios.post('http://localhost:7000/hostel/registration', formData)
       .then((response) => {
+        // alert
+        alert("Done")
         console.log('API response:', response.data);
       })
       .catch((error) => {
         console.error('API request error:', error);
       });
-      setFormData({
-        hostel_name: '',
-        description:'',
-        address1:'',
-        address2:'',
-        country:'',
-        state:'',
-        region:'',
-        district:'',
-        city:'',
-        pincode:'',
-        uuid:'',
-        rector_name:'',
-        categ1:'',
-        categ2:'',
-        categ3:'',
-        tower:'',
-        floor:'',
-        room:'',
-        scapacity:'',
-        bcapacity:'',
-        area:'',
-        mess:'',
-        other_facility:'',
-        status:'',
-        email_id:'',
-        website:'',
-      });
+      // setFormData({
+      //   hostel_name: '',
+      //   description:'',
+      //   address1:'',
+      //   address2:'',
+      //   country:'',
+      //   state:'',
+      //   region:'',
+      //   district:'',
+      //   city:'',
+      //   pincode:'',
+      //   uuid:'',
+      //   rector_name:'',
+      //   categ1:'',
+      //   categ2:'',
+      //   categ3:'',
+      //   tower:'',
+      //   floor:'',
+      //   room:'',
+      //   scapacity:'',
+      //   bcapacity:'',
+      //   area:'',
+      //   mess:'',
+      //   other_facility:'',
+      //   status:'',
+      //   email_id:'',
+      //   website:'',
+      // });
       setErrors({});
     } else {
       console.log("error");
@@ -207,7 +209,7 @@ const App = () => {
   return (
     <div className="flex">
       {/* Side Navbar */}
-      <div className="w-1/6 bg-orange-400">
+      <div className="w-1/6 bg-accent">
 
       </div>
 
@@ -215,64 +217,64 @@ const App = () => {
       <div className="w-5/6 h-full">
       <form onSubmit={handleSubmit}>
         {/* Header */}
-        <div className='w-full bg-cyan-300 h-36 flex '>
-          <div className='h-2/3 bg-white w-1/2 my-auto ml-10 drop-shadow-xl border-none rounded-lg'>
-            <p className='pl-10 pt-2 text-sky-800 font-bold text-3xl '>Hostel Mangement System 🎉</p>
-            <p className='pl-10 pt-1 text-orange-400 font-semibold text-2xl'>Hostel Registration Form</p>
+        <div className='w-full bg-defaultBg h-36 flex '>
+          <div className='h-auto bg-white w-11/12 my-auto ml-10 drop-shadow-xl border-none rounded-lg font-popins'>
+            <p className='pl-10 pt-4  text-sky-800 font-extrabold text-3xl font-popins'>Hostel Mangement System 🎉</p>
+            <p className='pl-10 pb-4 text-orange-400 font-semibold text-2xl font-popins'>Hostel Registration Form</p>
           </div>
         </div>
 
         {/* Form Data */}
-        <div className='w-full h-full bg-white flex justify-center '>
-          <div className="bg-orange-100 w-11/12 h-auto mt-10 border-none rounded-lg flex justify-center">
-            <div className='w-5/6 h-auto mt-5 mb-5 ml-6 mr-6 flex flex-col'>
+        <div className='w-full h-full bg-defaultBg flex justify-center font-popins'>
+          <div className="bg-white w-11/12 h-auto mt-10 border-none rounded-lg flex justify-center font-popins">
+            <div className='w-5/6 h-auto mt-5 mb-5 ml-6 mr-6 flex flex-col font-popins'>
               {/* 1 --> Basic Details */}
-              <div className='font-bold underline  underline-offset-1 text-sky-950 text-xl mb-3'>
-                <p className=''>Basic Details</p>
+              <div className='font-semibold underline  underline-offset-1 text-sky-950 text-3xl pt-3 mb-3 font-popins'>
+                <p className='font-popins'>Basic Details</p>
               </div>
               
               {/* 1.1 */}
-              <div className='w-full h-auto flex flex-col mt-3'>
-                <div className="mb-1 " htmlFor="description">Hostel Name *</div>
+              <div className='w-full h-auto flex flex-col mt-3 font-popins'>
+                <div className="mb-1 font-popins text-xl font-medium  " htmlFor="description">Hostel Name *</div>
                 <input
                   id="hostel_name"
                   name="hostel_name"
                   value={formData.hostel_name}
                   onChange={handleChange}
-                  className='w-full border-2 border-sky-500 rounded-md'
+                  className='w-full border border-borderColor rounded-md font-montserrat px-1 py-1'
                 ></input>
                 {errors.hostel_name && <span className="error">{errors.hostel_name}</span>}
               </div>
 
               {/* 1.2 */}
-              <div className='w-full h-auto flex flex-col mt-3'>
-                <div className="mb-1 " htmlFor="description">Hostel Description *</div>
+              <div className='w-full h-auto flex flex-col mt-3 font-popins'>
+                <div className="mb-1 font-popins" htmlFor="description">Hostel Description *</div>
                 <textarea
                   id="description"
                   name="description"
                   value={formData.description}
                   onChange={handleChange}
-                  className='w-full border-2 border-sky-500 rounded-md'
+                  className='w-full border-2 border-sky-500 rounded-md font-popins'
                 ></textarea>
                 {errors.description && <span className="error">{errors.description}</span>}
               </div>
 
             {/* 2 --> Address*/}
             <div className='font-bold underline  underline-offset-1 text-sky-950 text-xl mt-6 mb-3'>
-                <p className=''>Address </p>
+                <p className='font-popins'>Address </p>
             </div>
             {/* 2.1 */}
 
             <div className='w-full h-auto flex justify-between'>
                 <div className='w-1/2'>
-                  <div className="mb-1 " htmlFor="email_id">Address Line 1 **</div>
+                  <div className="mb-1 font-popins" htmlFor="email_id">Address Line 1 **</div>
                     <input
                       type="text"
                       id="address1"
                       name="address1"
                       value={formData.address1}
                       onChange={handleChange}
-                      className='w-11/12 border-2 border-sky-500 rounded-md'
+                      className='w-11/12 border-2 border-sky-500 rounded-md font-popins'
                     />
                     {errors.address1 && <span className="error">{errors.address1}</span>}
                 </div>
@@ -665,8 +667,8 @@ const App = () => {
         </div>
 
         {/* Footer - Logo + Submit button*/}
-        <div className='w-full h-52 flex items-center justify-center'>
-          <div className='w-11/12 h-4/5 flex items-center justify-center'>
+        <div className='w-full h-52 flex items-center justify-center bg-defaultBg'>
+          <div className='w-11/12 h-4/5 flex items-center justify-center bg-defaultBg'>
             <div className='w-11/12 h-1/2 flex justify-between'>
               <div className='w-auto flex flex-col justify-center items-start'>
                 {/* Logo */}
@@ -684,8 +686,6 @@ const App = () => {
 
       </form>
       </div>
-
-     
     </div>
 
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import HostelRegistration from './Pages/HostelRegistration';
+import TowerRegistartion from './Pages/TowerRegistartion';
+import RoomRegistartion from './Pages/RoomRegistartion';
+
 import './index.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path="/hostel-Registration" element={<HostelRegistration />} />
+        <Route path="/tower-Registration" element={<TowerRegistartion />} />
+        <Route path="/room-Registration" element={<RoomRegistartion />} />
+      </Routes>
+  </BrowserRouter>
 );
 

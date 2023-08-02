d5ba1bd9b12b2269d9087fe6e47c70813b49cf2b
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-08-02 19:36:10 +0530

    ReviewCard

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -32,7 +32,7 @@ const Dashboard = () => {
                         <p className='font-popins font-semibold overflow-x-hidden'>Rajesh <br />Bhatachariya</p>
                     </div>
                 </div>
-
+                
                 {/* Info */}
                 <div className='w-full justify-center mt-4'>
                     <p className='w-full text-center font-popins text-xs text-slate-700'>IIT Bombay, Hostel Rector</p>
--- frontend/src/Pages/HostelReviewForm.js
+++ frontend/src/Pages/HostelReviewForm.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios, { all } from 'axios';
+import Dropdown from 'react-dropdown';
+import DashboardImg from '../Components/DashboardImg.svg'
+import BgImg from './grid.svg'
+import Select from 'react-select';
+import { useHistory } from 'react-router-dom';
 
 const HostelReviewForm = () => {
-  return (
-    <div>HostelReviewForm</div>
-  )
-}
+
+    const [currentDate, setCurrentDate] = useState('');
+    const [currentTime, setCurrentTime] = useState('');
+
+    const [allForms,setAllForms]=useState([]);
+
+    const getAllSavedForms =async()=>{
+        try{
+            const response = await fetch('http://localhost:7000/hostel/registration/saved', {
+                method: 'GET',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            });
+            if(response.ok) {
+                console.log(response);
+            }
+        }
+        catch(err){
+            alert(err);
+        }
+    }
+
+    useEffect(()=>{
+        getAllSavedForms();
+    },[])
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        const date = new Date();
+  
+        const formattedDate = formatDate(date);
+        const formattedTime = formatTime(date);
+  
+        setCurrentDate(formattedDate);
+        setCurrentTime(formattedTime);
+      }, 1000);
+  
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, []);
+  
+    // Helper function to format the date
+    const formatDate = (date) => {
+      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+    };
+  
+    // Helper function to format the time
+    const formatTime = (date) => {
+      const options = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+      return date.toLocaleTimeString(undefined, options);
+    };
+
+    return (
+        <div className="w-full bg-defaultBg top-0">
+          {/* Header */}
+          <div className='w-full flex justify-center h-1/2 pt-10' >
+              <div className='flex flex-row w-11/12 h-40 bg-white rounded-lg drop-shadow-lg'>
+                  {/* content */}
+                  <div className='w-1/2 flex flex-col ml-5'>
+                      <div className='w-full mt-5'>
+                          <p className='font-popins text-2xl font-semibold '>Hostel Management Software</p>
+                      </div>
+                      <div className='w-full mt-1'> 
+                          <p className='font-popins text-lg font-medium text-orange-500 '>Saved Hostel registration Forms</p>
+                      </div>
+  
+                      <div className='w-full mt-3'>
+                          <p className='font-popins text-ms '>👋🏻 Hello <p className='inline font-bold'>Rajesh</p>, Welcome to your dashboard 🎉</p>
+                      </div>
+                      <div className='w-full mt-0.5 mb-5'>
+                          <p className='font-popins text-ms '>🗓️ {currentDate}  | 🕛 {currentTime}</p>
+                      </div>
+                  </div>
+  
+                  {/* Image */}
+                  <div className='w-1/2 flex justify-end mr-5 '>
+                  <img src={DashboardImg} alt="Circular" className='w-25 h-22 pt-4 pb-4'/>
+                  </div>
+              </div>
+              </div>
+          </div>
+    );
+  };
+  
 
 export default HostelReviewForm

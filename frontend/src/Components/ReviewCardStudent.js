cdbf33213728b21ed06d1afd3df7260d11de3209
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-08-31 18:34:47 +0530

    ReviewCard Student Done

--- frontend/src/Components/ReviewCardStudent.js
+++ frontend/src/Components/ReviewCardStudent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import HostelImage from '../utils/Hostel_Image.svg'
 const ReviewCard = (props) => {
     const [formData,setFormData]=useState(props.data);
     const setAll=()=>{
@@ -17,45 +17,83 @@ const ReviewCard = (props) => {
         props.setDisplay(true);
     }
   return (
-    <div className='w-full flex mt-10 mb-10 border-0 rounded-lg'>
+    <div className='w-full auto bg-white flex mt-10 mb-10 border-0 rounded-tl-lg rounded-bl-lg rounded-tr-lg rounded-br-lg'>
         {/* Details */}
-        <div className='w-4/5 bg-gray-200 flex-col p-3'>
+        <div className='w-3/5 flex-col p-3 mb-5'>
             {/* Hostel name */}
-            <div className='mb-1 font-popins text-medium text-blue-500 font-medium flex justify-start text-lg'>
-                <div>Name :<p className='inline ml-2 text-black'>{formData.first_name} {formData.last_name} ({formData.student_id})</p></div>
+            <div className='mb-1 font-popins text-medium text-black font-medium flex-col justify-start mt-3 ml-4'>
+                <div className='text-lg '>Name :</div>
+                <div><p className='text-blue-500  text-xl'>{formData.first_name} ({formData.last_name}) </p></div>
+                {/* <div className='ml-20'>Status :<p className='inline text-black'> {formData.status}</p></div> */}
             </div>
 
-            {/* Country and State */}
-            <div className='mb-1 font-popins text-medium text-blue-500 font-medium flex justify-start text-lg'>
-                <div>Gender :<p className='inline ml-5 text-black'>{formData.gender.toUpperCase()}</p></div>
-                <div className='ml-40'>DOB :<p className='inline ml-5 text-black'>{formData.dob.split('T')[0]}</p></div>
+
+            <div className='mb-1 font-popins text-medium text-black font-medium flex-col justify-start mt-2 ml-4'>
+                <div className='flex'>
+                <div className='flex-col'>
+                    <div className='text-lg '>Gender :</div>
+                    <div>
+                        <p className='text-blue-500  text-xl'>{formData.gender.toUpperCase()}</p>
+                    </div>
+                </div>
+                <div  className='flex-col ml-10'>
+                    <div className='text-lg '>DOB :</div>
+                    <div>
+                        <p className='text-blue-500  text-xl'>{formData.dob}</p>
+                    </div>
+                </div>
+                </div>
+                
+               
             </div>
 
-            {/* Country and State */}
-            <div className='mb-1 font-popins text-medium text-blue-500 font-medium flex justify-start text-lg'>
-                <div>State :<p className='inline ml-5 text-black'>{formData.state}</p></div>
-                <div className='ml-16'>Taluka :<p className='inline ml-5 text-black'>{formData.city}</p></div>
+            <div className='mb-1 font-popins text-medium text-black font-medium flex-col justify-start mt-2 ml-4'>
+                <div className='flex'>
+                <div className='flex-col'>
+                    <div className='text-lg '>District:</div>
+                    <div>
+                        <p className='text-blue-500  text-xl'>{formData.state}</p>
+                    </div>
+                </div>
+                <div  className='flex-col ml-10'>
+                    <div className='text-lg '>Taluka:</div>
+                    <div>
+                        <p className='text-blue-500  text-xl'>{formData.city}</p>
+                    </div>
+                </div>
+                </div>
             </div>
 
+            <div className='flex w-4/5 justify-start mt-5'>
+            <div>   
+                <button className='bg-defaultBg w-36 h-10 border-1 border-slate-400 rounded-md'
+                    onClick={setAll}
+                >
+                    <p className='font-popins text-lg text-normal'>Edit</p>
+                </button>
+            </div>
+            <div>  
+                <button className='bg-defaultBg w-36 h-10 border-1 border-slate-400 rounded-md ml-5'
+                    onClick={setDisplay}
+                >
+                    <p className='font-popins text-normal'>View</p>
+                </button>
+            </div>
+            </div>
         </div>
 
         {/* Buttons */}
-        <div className='w-1/5 bg-stone-400 pt-2 pb-2'>
-            <button className='bg-defaultBg w-32 h-8 border-2 border-slate-950 rounded-l-full rounded-r-full ml-10'
-                onClick={setAll}
-            >
-                <p className='font-popins text-normal'>Edit</p>
-            </button>
-            {/* <button className='bg-defaultBg w-32 h-8 border-2 border-slate-950 rounded-l-full rounded-r-full ml-10 mt-2'
-                onClick={setEdi2}
-            >
-                <p className='font-popins text-normal'>Review</p>
-            </button> */}
-            <button className='bg-defaultBg w-32 h-8 border-2 border-slate-950 rounded-l-full rounded-r-full ml-10 mt-2'
-                onClick={setDisplay}
-            >
-                <p className='font-popins text-normal'>View</p>
-            </button>
+        <div className='w-2/5 mt-10 flex justify-end pr-5 mr-'>
+            <div className='w-full flex-col'>
+                {/* <div className='flex justify-end'>
+                    <button className={`w-3/5 h-10 flex justify-center pt-1 rounded-md pl-2 pr-2`} disabled={true}>
+                        <p className='text-xl font-bold'>{}</p>
+                    </button>
+                </div> */}
+                <div className='flex justify-end'>
+                    <img src={HostelImage} className='w-3/5 h-auto' />
+                </div>
+            </div>
         </div>
     </div>
   )
